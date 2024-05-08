7e6f2c9fa44effd76082c3ba48203cdf284a59a1
heega <m_mny1004@naver.com>
2024-05-08 11:02:44 +0900

    farmstory

---
+++ farmstory/src/pages/main/MainPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
+import Main from "../../components/main/Main";
+import DefaultLayout from "../../layouts/DefaultLayout";
+
+const MainPage = () => {
+  return (
+    // <div id="container">
+    //   <Header />
+    //   <Main />
+    //   <Footer />
+    // </div>
+    <DefaultLayout>
+      <Main />
+    </DefaultLayout>
+  );
+};
+
+export default MainPage;
